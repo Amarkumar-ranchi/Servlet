c0fd9bbfe6b26b0ee6d7da6924bbcb5c5c11a672
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2025-04-16 19:00:23 +0530

    Add files via upload

--- DemoServlet.java
+++ DemoServlet.java
@@ -1,9 +1,6 @@
 package com.jlcindia.servlets; 
- 
+
 import java.io.IOException; 
-import java.util.ArrayList; 
-import java.util.List; 
- 
 import javax.servlet.RequestDispatcher; 
 import javax.servlet.ServletException; 
 import javax.servlet.annotation.WebServlet; 
@@ -11,22 +8,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest; 
 import javax.servlet.http.HttpServletResponse; 
 import javax.servlet.http.HttpSession; 
-
 @WebServlet(name = "mydemo", urlPatterns = "/mydemo.jlc") 
 public class DemoServlet extends HttpServlet { 
- 
- public void service(HttpServletRequest request, HttpServletResponse response) 
-throws IOException, ServletException { 
-  System.out.println("---DemoServlet---"); 
- 
 
-HttpSession Session=request.getSession();
-System.out.println(Session.isNew()); 
-System.out.println(Session.getId()); 
+public void service(HttpServletRequest request, HttpServletResponse response) throws 
+IOException, ServletException { 
+System.out.println("----DemoServlet---"); 
 
-  RequestDispatcher rd = request.getRequestDispatcher("index.html"); 
-  rd.forward(request, response); 
- } 
+HttpSession session = request.getSession(); 
+System.out.println(session.isNew()); 
+System.out.println(session.getId()); 
+
+RequestDispatcher rd = request.getRequestDispatcher("show.html"); 
+rd.forward(request, response); 
+} 
 } 
  
  
