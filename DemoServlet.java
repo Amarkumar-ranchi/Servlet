bf58ec307b583117fe4230e1e80ac09094b6be6e
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2025-04-16 18:59:42 +0530

    Add files via upload

--- DemoServlet.java
+++ DemoServlet.java
@@ -1,86 +1,35 @@
 package com.jlcindia.servlets; 
  
-import java.io.*; 
-import java.util.*; 
+import java.io.IOException; 
+import java.util.ArrayList; 
+import java.util.List; 
  
-import javax.servlet.http.*; 
+import javax.servlet.RequestDispatcher; 
 import javax.servlet.ServletException; 
 import javax.servlet.annotation.WebServlet; 
+import javax.servlet.http.HttpServlet; 
+import javax.servlet.http.HttpServletRequest; 
+import javax.servlet.http.HttpServletResponse; 
+import javax.servlet.http.HttpSession; 
 
-  
+@WebServlet(name = "mydemo", urlPatterns = "/mydemo.jlc") 
+public class DemoServlet extends HttpServlet { 
  
-@WebServlet(name="demo",urlPatterns = {"/mydemo.jlc"}) 
-public class DemoServlet  extends HttpServlet{ 
+ public void service(HttpServletRequest request, HttpServletResponse response) 
+throws IOException, ServletException { 
+  System.out.println("---DemoServlet---"); 
  
-public void service(HttpServletRequest request, HttpServletResponse response) throws 
-IOException,ServletException{ 
- 
-//1.Request Parameters 
-System.out.println("-----1.Request Parameters-----"); 
-Enumeration<String>  enms1 = request.getParameterNames(); 
-List<String> paramNames = Collections.list(enms1); 
- 
-for(String paramName: paramNames ) { 
-String paramValue =request.getParameter(paramName); 
-System.out.println(paramName+"-----\t"+paramValue); 
+
+HttpSession Session=request.getSession();
+System.out.println(Session.isNew()); 
+System.out.println(Session.getId()); 
+
+  RequestDispatcher rd = request.getRequestDispatcher("index.html"); 
+  rd.forward(request, response); 
+ } 
 } 
  
-//2.Request Headers 
-System.out.println("-----2.Request Headers-----"); 
-Enumeration<String>  enms2 = request.getHeaderNames(); 
-List<String> headerNames = Collections.list(enms2); 
- 
-for(String headerName : headerNames ) { 
-String headerValue =request.getHeader(headerName); 
-System.out.println(headerName+"-----\t"+headerValue); 
-} 
- 
-//2.Request Headers 
-System.out.println("-----2.Request Headers-----"); 
-String hvalue =request.getHeader("referer"); 
-System.out.println(hvalue); 
-hvalue =request.getHeader("accept-encoding"); 
-System.out.println(hvalue); 
-hvalue =request.getHeader("cookie"); 
-System.out.println(hvalue); 
  
  
  
- 
- 
- 
- 
- 
-//3.Request Cookies 
-System.out.println("-----3.Request Cookies-----"); 
-Cookie cks[] = request.getCookies(); 
-if(cks!=null) { 
-for(Cookie ck:cks) { 
-System.out.println(ck.getName()+"-------"+ck.getValue()); 
-} 
-} 
- 
-//4.Other Information 
-System.out.println("-----4.Other Information-----"); 
-System.out.println(" getRequestURI : "+request.getRequestURI()); //Imp 
-System.out.println("Http Method : "+request.getMethod()); 
-System.out.println("getLocalAddr : "+request.getLocalAddr()); 
-System.out.println(" getLocalAddr: "+request.getLocalAddr()); 
-System.out.println(" getRemoteAddr: "+request.getRemoteAddr()); 
-System.out.println("getLocale : "+request.getLocale().getLanguage()); 
-System.out.println("getAuthType : "+request.getAuthType()); 
-System.out.println(" getCharacterEncoding: "+request.getCharacterEncoding()); 
-System.out.println(" getContentLength: "+request.getContentLength()); 
-System.out.println(" getContentType: "+request.getContentType()); 
-System.out.println("getPathInfo : "+request.getPathInfo()); 
-System.out.println(" getQueryString: "+request.getQueryString()); 
-System.out.println(" getRemoteHost: "+request.getRemoteHost()); 
-System.out.println(" getRemoteUser: "+request.getRemoteUser()); 
-System.out.println(" getServletPath : "+request.getServletPath()); 
- 
-response.setContentType("text/html"); 
-PrintWriter out= response.getWriter(); 
-out.println("<h1> Check the Console for output</h1>"); 
-} 
-} 
  
