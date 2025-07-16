59a9effa1c93759a1bef4fa173dfc8dd86f9b0f5
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2025-07-16 14:48:40 +0530

    Add files via upload

--- DemoServlet.java
+++ DemoServlet.java
@@ -1,8 +1,12 @@
-package com.jlcindia.servlets;  
-import java.io.*; 
-import javax.servlet.*; 
+ 
+package com.jlcindia.servlets; 
+ 
+import java.io.IOException; 
+import java.io.PrintWriter; 
+ 
+import javax.servlet.RequestDispatcher; 
+import javax.servlet.ServletException; 
 import javax.servlet.annotation.WebServlet; 
-import javax.servlet.http.Cookie; 
 import javax.servlet.http.HttpServlet; 
 import javax.servlet.http.HttpServletRequest; 
 import javax.servlet.http.HttpServletResponse; 
@@ -13,42 +17,29 @@ public class DemoServlet extends HttpServlet {
  
 public void service(HttpServletRequest request, HttpServletResponse response) throws 
 IOException, ServletException { 
-System.out.println("----DemoServlet---"); 
+System.out.println("1."+"----DemoServlet---"); 
  
-Cookie cks[] = request.getCookies(); 
- 
-if(cks!=null) { 
-System.out.println("No. of Cookies : "+cks.length); 
- 
-for(Cookie mycookie:cks) { 
-String cname=mycookie.getName(); 
-String cvalue=mycookie.getValue(); 
-System.out.println(cname+"\t"+cvalue); 
-} 
- 
-}else { 
-System.out.println("ooops No Cookies Found : "); 
-} 
 HttpSession session = request.getSession(); 
+System.out.println("2."+session.getId()); 
+ 
+boolean b1=request.isRequestedSessionIdFromCookie(); 
+System.out.println("3."+b1); 
+boolean b2=request.isRequestedSessionIdFromURL(); 
+System.out.println("4."+b2); 
+ 
+String page=""; 
 if(session.isNew()) { 
-Cookie ck1=new Cookie("myemail","sri@jlc"); 
-response.addCookie(ck1); 
- 
-Cookie ck2=new Cookie("myphone","12345"); 
-response.addCookie(ck2); 
-ck2.setMaxAge(60);  
+page="demo.jsp"; 
 }else { 
-Cookie ck3=new Cookie("hello","Hello-Guys"); 
-response.addCookie(ck3); 
-ck3.setMaxAge(0); 
+page="home.jsp"; 
 } 
  
-PrintWriter out=response.getWriter(); 
-out.println("<h1>Check the Console Output</h1>"); 
+System.out.println("5."+page); 
+page=response.encodeRedirectURL(page); 
+System.out.println("6."+page); 
+ 
+ 
+RequestDispatcher rd= request.getRequestDispatcher(page); 
+rd.forward(request, response); 
 } 
 } 
- 
- 
- 
- 
- 
--- LoginServlet.java
+++ LoginServlet.java
@@ -1,38 +1,47 @@
-package com.jlcindia.servlets;
-
-
+package com.jlcindia.servlets; 
+ 
 import java.io.IOException; 
-import java.io.PrintWriter;
+import javax.servlet.*; 
+import javax.servlet.annotation.WebServlet; 
+import javax.servlet.http.*; 
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet; 
-import javax.servlet.http.HttpServletRequest; 
-import javax.servlet.http.HttpServletResponse; 
-@WebServlet(name="login",urlPatterns= {"/mylogin.jlc","/login.jlc"})
+ 
+@WebServlet(name="login",urlPatterns = {"/mylogin.jlc","/login.jlc"}) 
 public class LoginServlet  extends HttpServlet{ 
-
+ 
 public void service(HttpServletRequest request, HttpServletResponse response) throws 
 IOException,ServletException{ 
-
+ 
 //1.Collect Input Data 
 String un = request.getParameter("myusername"); 
 String pw = request.getParameter("mypassword"); 
-
-
-//2.Do Processing and Prepare Response 
-String page="";
-if(un.equals(pw)) {
-	request.setAttribute("UN", un);
-	page="home.jsp";
-}else {
-	String msg="Invalid Username or Password";
-	request.setAttribute("MSG", msg);
-	page="login.jsp";
-}
-RequestDispatcher rd= request.getRequestDispatcher(page);
-rd.forward(request, response);
-
+String rem = request.getParameter("remember"); 
+System.out.println(rem); 
+ 
+//2.Do Processing and Prepare the Response 
+String page=""; 
+if(un.equals(pw)) { 
+//Success 
+page="home.jsp"; 
+request.setAttribute("UN", un); 
+if(rem!=null) { 
+if(rem.equals("Yes")) { 
+Cookie ck1=new Cookie("UNAME",un); 
+ck1.setMaxAge(365*24*60); 
+response.addCookie(ck1); 
+ 
+Cookie ck2=new Cookie("PWORD",pw); 
+ck2.setMaxAge(365*24*60); 
+response.addCookie(ck2); 
+} 
+} 
+}else { 
+page="login.jsp"; 
+String msg= " <h3>Login Failed - Try again</h3>"; 
+request.setAttribute("ErrMsg", msg); 
+} 
+//3.Forward 
+RequestDispatcher rd=request.getRequestDispatcher(page); 
+rd.forward(request, response); 
 } 
 } 
